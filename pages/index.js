a5d11c0696f40cf576dda889234a5b9755223dc6
Anthony Ingle <ingle.anthony@icloud.com>
2021-08-15 00:47:43 -0400

    Refactored

--- pages/index.js
+++ pages/index.js
@@ -12,22 +12,55 @@ export default function Home() {
   const [completed, setCompleted] = useState(false);
 
   useEffect(() => {
-    const script = document.createElement("script");
-
-    script.src = "/sib.js";
-    script.async = true;
-
-    document.body.appendChild(script);
+    // sendinblue script
+    (function () {
+      window.sib = {
+        equeue: [],
+        client_key: "g9ud0g01fg7x7vlzq0m2p9kz",
+      };
+      /* OPTIONAL: email for identify request*/
+      // window.sib.email_id = 'example@domain.com';
+      window.sendinblue = {};
+      for (
+        var j = ["track", "identify", "trackLink", "page"], i = 0;
+        i < j.length;
+        i++
+      ) {
+        (function (k) {
+          window.sendinblue[k] = function () {
+            var arg = Array.prototype.slice.call(arguments);
+            (
+              window.sib[k] ||
+              function () {
+                var t = {};
+                t[k] = arg;
+                window.sib.equeue.push(t);
+              }
+            )(arg[0], arg[1], arg[2]);
+          };
+        })(j[i]);
+      }
+      var n = document.createElement("script"),
+        i = document.getElementsByTagName("script")[0];
+      (n.type = "text/javascript"),
+        (n.id = "sendinblue-js"),
+        (n.async = !0),
+        (n.src =
+          "https://sibautomation.com/sa.js?key=" + window.sib.client_key),
+        i.parentNode.insertBefore(n, i),
+        window.sendinblue.page();
+    })();
   }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setLoading(true);
+    window.sib.email_id = event.target.email.value;
 
-    var myHeaders = new Headers();
+    let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
-    var requestOptions = {
+    let requestOptions = {
       method: "POST",
       mode: "same-origin",
       headers: myHeaders,
